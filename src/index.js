fe2c7faa921f670ad81a24b86ccfad116f6ef2c7
liu.hanru <isuekey@github.com>
2019-07-22 01:12:14 +0800

    click texture

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import * as three from '../node_modules/three/build/three.module.js';
 
 import appCss from './app.css';
 import back from './back2.jpeg';
-import nx from './nx.png';
-import gcb from './coin_back.jpg';
-import gcbd from './coin_border.png';
-import gcf from './coin_front.jpg';
+import frontTextrue from './front_texture.png';
+import rightTexture from './right_texture.png';
+import backTexture from './back_texture.png';
+import leftTexture from './left_texture.png';
+import topTexture from './top_texture.png';
+import bottomTexture from './bottom_texture.png';
 
+const unit = 15;
+const wall = Object.freeze({
+  top:[0, unit, 0],
+  front:[0, 0, unit],
+  right:[-unit, 0, 0],
+  back:[0, 0, -unit],
+  left:[unit, 0, 0],
+  bottom:[0, -unit, 0]
+});
 
 class CubeMapGenerator {
   constructor(renderer) {
@@ -90,13 +101,11 @@ class MyScene {
     this.scene.depth = true;
 
     this.cubeCamera1 = this.initCubeCamera(this.scene);
-    this.cubeCamera2 = this.initCubeCamera(this.scene);
     this.camera =  new three.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 
       1, 1000);
-    const position = [0, 0, 20];
+    const position = [0, 0, 10];
     this.camera.position.set(...position);
     this.cameraDistance = Math.sqrt(position.reduce((sum, cur) => sum + cur * cur));
-    this.mpaUrl = nx;
     this.backgroundUrl = back;
     this.lon = 0;
     this.lat = 0;
@@ -107,6 +116,10 @@ class MyScene {
     this.onPointerDownPointerY=0;
     this.onPointerDownLon=0;
     this.onPointerDownLat=0;
+
+    this.mouse = new three.Vector2();
+    this.raycaster = new three.Raycaster();
+    this.INTERSECTED = undefined;
   }
   initCubeCamera(scene) {
     const cubeCamera = new three.CubeCamera(3, 1000, 256);
@@ -121,37 +134,68 @@ class MyScene {
       loader.load(textureUrl, resolve, reject);
     });
   }
+
+  insertFlatShape(scene, whichWall=wall.front, materailConfig={color:0x00ff00} , selectedCaller,position=[0,0], size=[1, 1]) {
+    let [width, height] = size;
+    let [x, y] = position;
+    let shape = new three.Shape();
+    shape.moveTo(x, y);
+    shape.lineTo(x+width, y);
+    shape.lineTo(x+width, y+height);
+    shape.lineTo(x, y+height);
+    shape.lineTo(x, y);
+    const matrix = new three.Matrix4();
+    const rotation = new three.Matrix4();
+    const scale = new three.Matrix4();
+    scale.makeScale(unit, unit, unit);
+    const [tx, ty, tz] = whichWall;
+    !!tx && rotation.makeRotationY(-Math.PI * tx / unit / 2);
+    !!ty && rotation.makeRotationX(Math.PI * (ty / unit / 2 ));
+    !!tz && rotation.makeRotationY(Math.PI * (tz / unit + 1) /2);
+    matrix.makeTranslation(tx, ty, tz);
+    matrix.multiply(rotation);
+    matrix.multiply(scale);
+    if (materailConfig.map) {
+      materailConfig.map.wrapS = three.ClampToEdgeWrapping;
+      materailConfig.map.wrapT = three.ClampToEdgeWrapping;
+    }
+    const shapeGeometry = new three.ShapeGeometry(shape);
+    shapeGeometry.applyMatrix(matrix);
+    const material = new three.MeshBasicMaterial(materailConfig);
+    const shapeMesh = new three.Mesh(shapeGeometry, material);
+    shapeMesh.selectedCaller = selectedCaller || (() => {console.log('not define the click event: selectedCaller')})
+    scene.add(shapeMesh);
+  }
   
   begin() {
     const loadBackground = this.loadTexture(back);
-    const loadCubeTexture = this.loadTexture(gcb);
-    const loadBorderTexture = this.loadTexture(gcbd);
-    const loadFrontTexture = this.loadTexture(gcf);
-    return Promise.all([loadBackground, loadCubeTexture, loadBorderTexture, loadFrontTexture]).then(textureArray => {
+    const loadtop = this.loadTexture(topTexture);
+    const loadfront = this.loadTexture(frontTextrue);
+    const loadright = this.loadTexture(rightTexture);
+    const loadback = this.loadTexture(backTexture);
+    const loadleft = this.loadTexture(leftTexture);
+    const loadbottom = this.loadTexture(bottomTexture);
+    return Promise.all([loadBackground, loadtop, loadfront, loadright, loadback,loadleft, loadbottom]).then(textureArray => {
       const backgroundTexture = textureArray[0];
       const options = {
         resolution: 1024,
-
         generateMipmaps: true,
         minFilter: three.LinearMipMapLinearFilter,
         magFilter: three.LinearFilter
       };
+
       this.scene.background = new CubeMapGenerator(this.renderer).fromEquirectangular(backgroundTexture, options);
 
-      const cubeTexure = textureArray[1];
-      cubeTexure.wrapS = three.RepeatWrapping;
-      cubeTexure.wrapT = three.RepeatWrapping;
+      const cubeTexture = textureArray[1];
+      this.insertFlatShape(this.scene, wall.top, {map:textureArray[1]});
+      this.insertFlatShape(this.scene, wall.front, {map:textureArray[2]}, ()=> {
+        console.log('this is insert a flat shape when');
+      });
+      this.insertFlatShape(this.scene, wall.right, {map:textureArray[3]});
+      this.insertFlatShape(this.scene, wall.back, {map:textureArray[4]});
+      this.insertFlatShape(this.scene, wall.left, {map:textureArray[5]});
+      this.insertFlatShape(this.scene, wall.bottom, {map:textureArray[6]});
 
-      const backMaterial = new three.MeshBasicMaterial({map: cubeTexure});
-      const borderMaterial = new three.MeshBasicMaterial({map: textureArray[2]});
-      const frontMaterial = new three.MeshBasicMaterial({map: textureArray[3]});
-      // const geometry = new three.BoxGeometry(1, 1, 1);
-      const geometry = new three.CylinderBufferGeometry(5, 5, 1, 64);
-      const cube = new three.Mesh(geometry, [borderMaterial, frontMaterial, backMaterial]);
-      cube.rotation.x = Math.PI/ 3;
-      cube.rotation.y = Math.PI/ 2;
-      this.scene.add(cube);
-      // this.camera.position.z = 0;
       document.addEventListener('mousedown', this.onDocumentMouseDown.bind(this), false);
       document.addEventListener('touchstart', this.onDocumentTouchStart.bind(this), false);
       document.addEventListener('wheel', this.onDocumentMouseWheel.bind(this), false);
@@ -189,24 +233,37 @@ class MyScene {
     document.addEventListener( 'touchleave', this.onDocumentMouseUp.bind(this), false );
   }
   onDocumentTouchMove( touchEvent ) {
+    // event.preventDefault();
     const event = touchEvent.touches[touchEvent.touches.length - 1];
     this.lon = ( event.clientX - this.onPointerDownPointerX ) * 0.1 + this.onPointerDownLon;
     this.lat = ( event.clientY - this.onPointerDownPointerY ) * 0.1 + this.onPointerDownLat;
+    this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    // this.mouse.x = (this.lon / window.innerWidth) * 2 - 1;
+    // this.mouse.y = (this.lat / window.height) * 2 - 1;
   }
 
   onDocumentTouchLeave() {
     document.removeEventListener( 'touchmove', this.onDocumentTouchMove.bind(this), false );
     document.removeEventListener( 'touchleave', this.onDocumentTouchLeave.bind(this), false );
+    if(this.INTERSECTED && this.INTERSECTED.selectedCaller) {
+      this.INTERSECTED.selectedCaller();
+    }
   }
 
   onDocumentMouseMove( event ) {
     this.lon = ( event.clientX - this.onPointerDownPointerX ) * 0.1 + this.onPointerDownLon;
     this.lat = ( event.clientY - this.onPointerDownPointerY ) * 0.1 + this.onPointerDownLat;
+    this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
   }
 
   onDocumentMouseUp() {
     document.removeEventListener( 'mousemove', this.onDocumentMouseMove.bind(this), false );
     document.removeEventListener( 'mouseup', this.onDocumentMouseUp.bind(this), false );
+    if(this.INTERSECTED && this.INTERSECTED.selectedCaller) {
+      this.INTERSECTED.selectedCaller();
+    }
   }
 
   onDocumentMouseWheel( event ) {
@@ -220,8 +277,8 @@ class MyScene {
     this.render();
   }
   render() {
-    var time = Date.now();
-    this.lon += .15;
+    // var time = Date.now();
+    // this.lon += .15;
 
     this.lat = Math.max( - 85, Math.min( 85, this.lat ) );
     this.phi = three.Math.degToRad( 90 - this.lat );
@@ -232,7 +289,19 @@ class MyScene {
     this.camera.position.z = this.cameraDistance * Math.sin( this.phi ) * Math.sin( this.theta );
 
     this.camera.lookAt( this.scene.position );
-
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    const intersects = this.raycaster.intersectObjects( this.scene.children );
+    if ( intersects.length > 0 ) {
+      if ( this.INTERSECTED != intersects[ 0 ].object ) {
+        if ( this.INTERSECTED ) this.INTERSECTED.material.emissive.setHex( this.INTERSECTED.currentHex );
+        this.INTERSECTED = intersects[ 0 ].object;
+        this.INTERSECTED.currentHex = this.INTERSECTED.material.color.getHex();
+        this.INTERSECTED.material.color.setHex( 0xff0000 );
+      }
+    } else {
+      if ( this.INTERSECTED ) this.INTERSECTED.material.color.setHex( this.INTERSECTED.currentHex );
+      this.INTERSECTED = null;
+    }
     this.renderer.render( this.scene, this.camera );
 
   }
